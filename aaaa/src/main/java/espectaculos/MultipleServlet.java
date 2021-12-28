94c6bdac1ea32893d07484c6d2d82d6aab65eea8
alvarohghg <61513412+alvarohghg@users.noreply.github.com>
2021-12-28 01:39:07 +0100

    squick squick dijo el barsinso

--- aaaa/src/main/java/espectaculos/MultipleServlet.java
+++ aaaa/src/main/java/espectaculos/MultipleServlet.java
@@ -1,3 +1,4 @@
+/*Servlet para el registro de un espectaculo múltiple*/
 package espectaculos;
 
 import java.io.IOException;
@@ -17,7 +18,7 @@ import es.uco.pw.business.user.EspectaculoMultiple;
 import es.uco.pw.business.user.GestorEspectaculos;
 import es.uco.pw.business.user.AbstractEspectaculo.categoria;
 /**
- * Servlet implementation class RegistroUsuarios
+ * Servlet implementation class MultipleServlet
  */
 @WebServlet("/MultipleServlet")
 public class MultipleServlet extends HttpServlet {
@@ -44,6 +45,7 @@ public class MultipleServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//recogemos los datos de la pagina anterior
 		String titulo_mult = request.getParameter("titulo_mult"); 
 		String desc_mult = request.getParameter("desc_mult");
 		categoria categoria_mult = Enum.valueOf(categoria.class ,request.getParameter("categoria_mult"));
@@ -51,6 +53,7 @@ public class MultipleServlet extends HttpServlet {
 		int loc_mult = Integer.parseInt(request.getParameter("loc_mult"));
 		int num_fechas = Integer.parseInt(request.getParameter("nfechas"));
 		ArrayList<java.sql.Date> fechas_mult=new ArrayList<java.sql.Date>();
+		//Recogemos las sesiones del espectaculo en un array
 		for(int i=1;i<=num_fechas;i++) {
 				String pasando="Fecha" + i;
 				
@@ -68,7 +71,7 @@ public class MultipleServlet extends HttpServlet {
 		//Introducimos lo valores en la base de datos
 		response.setContentType("text/html");
 		PrintWriter salida= response.getWriter();
-		if(GE.existeEspectaculo(titulo_mult)) {
+		if(GE.existeEspectaculo(titulo_mult)) { //si existe mostramos mensaje de error
 			salida.println("<HTML> <body style='background-color: black' ><script defer type=\"text/javascript\">");
 			salida.println("alert('Error al crear el espectaculo multiple');");
 			salida.println("location='/aaaa/index.jsp';");
